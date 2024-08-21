caf473e545500deb200e795d2da6b953982eddf2
Antonio Figueroa <antonio_figueroa@dell.com>
2024-08-21 14:00:47 -0500

    - completes method overloading exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -6,5 +6,29 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        static int Add (int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        static string Add(double num1, double num2, bool isCurrency)
+        {
+            string result = string.Empty;
+            if (isCurrency)
+            {
+                result = $"{num1 + num2} dollars";
+            } else
+            {
+                result = $"{num1 + num2}";
+            }
+
+            return result;
+        }
     }
 }
