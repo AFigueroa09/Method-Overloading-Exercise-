505fccf03e649636315a3f60ae37e8d46eacd1a2
Antonio Figueroa <antonio_figueroa@dell.com>
2024-08-23 13:30:42 -0500

    - calls overloaded methods in Main

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var add1 = Add(2, 3);
+            Console.WriteLine($"First sum method: 2 + 3 = {add1}");
+            var add2 = Add(2.51, 3.5);
+            Console.WriteLine($"Second sum method: 2.51 + 3.5 = {add2}");
+            var add3 = Add(2.51, 3.5, false);
+            Console.WriteLine($"third sum method, not currency: 2.51 + 3.5 ={add3}");
+            var add4 = Add(2.51, 3.5, true);
+            Console.WriteLine($"third sum method, is currency: 2.51 + 3.5 = {add4}");
         }
 
         static int Add (int num1, int num2)
